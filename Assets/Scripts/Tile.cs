0f1dfa9f2c833577c2c7fb232912f0361e82ee80
groobob <349578260@gapps.yrdsb.ca>
2022-12-23 20:02:52 -0500

    Added grid interactability

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -9,10 +9,20 @@ public class Tile : MonoBehaviour
     //Reference Variables
     [SerializeField] private Color baseColor, offsetColor;
     [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private GameObject highlight;
 
     public void SetOffsetColor(bool offset)
     {
         if(!offset) spriteRenderer.color = baseColor;
         else if(offset) spriteRenderer.color = offsetColor;
+    }
+    
+    void OnMouseEnter()
+    {
+        highlight.SetActive(true);
+    }
+    void OnMouseExit()
+    {
+        highlight.SetActive(false);
     }
 }
