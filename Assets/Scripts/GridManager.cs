cc93dfbf25ba06a1e4422aac44e5f8dc5f5f39cf
groobob <349578260@gapps.yrdsb.ca>
2022-12-25 00:21:08 -0500

    Added tile variants

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -8,9 +8,9 @@ public class GridManager : MonoBehaviour
 {
     //Reference Variables
     [SerializeField] private int width, height;
-    [SerializeField] private Tile gridTile;
+    [SerializeField] private Tile grassTile, mountainTile;
     [SerializeField] private Transform transformCamera;
-
+    private Dictionary<Vector2, Tile> tiles;
 
     void Start()
     {
@@ -18,18 +18,28 @@ public class GridManager : MonoBehaviour
     }
     void CreateGrid()
     {
+        tiles = new Dictionary<Vector2, Tile>();
         for(int x = 0; x < width; x++)
         {
             for(int y = 0; y < height; y++)
             {
-                var spawnedTile = Instantiate(gridTile, new Vector3(x, y), Quaternion.identity);
+                var randomTile = Random.Range(0, 6) == 3 ? mountainTile : grassTile;
+                var spawnedTile = Instantiate(randomTile, new Vector3(x, y), Quaternion.identity);
                 spawnedTile.name = $"tile {x} {y}";
 
                 var isOffset = (x + y) % 2 == 1;
                 spawnedTile.SetOffsetColor(isOffset);
+
+                tiles[new Vector2(x, y)] = spawnedTile;
             }
         }
 
         transformCamera.position = new Vector3(width / 2f - 0.5f, height / 2f - 0.5f);
+    }
+
+    public Tile GetTileAtPosition(Vector2 pos)
+    {
+        if(tiles.TryGetValue(pos, out var tile)) return tile;
+        return null;
     }
 }
