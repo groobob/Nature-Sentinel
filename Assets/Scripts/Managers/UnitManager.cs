89eb1b74bcff02cf713ff74c2671ae809ed0af09
groobob <349578260@gapps.yrdsb.ca>
2023-01-07 03:52:03 -0500

    Implemented random unit generation

--- Assets/Scripts/Managers/UnitManager.cs
+++ Assets/Scripts/Managers/UnitManager.cs
@@ -1,13 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class UnitManager : MonoBehaviour
 {
     public static UnitManager Instance;
 
+    private List<ScriptableUnit> _units;
     private void Awake()
     {
         Instance = this;
+
+        _units = Resources.LoadAll<ScriptableUnit>("units").ToList();
+    }
+
+    public void SpawnPlayers()
+    {
+        var playerCount = 1;
+
+        for(int i = 0; i < playerCount; i++)
+        {
+            var randomPrefab = GetRandomUnit<BasePlayer>(Faction.Player);
+            var spawnedPlayer = Instantiate(randomPrefab);
+        }
+    }
+
+    private T GetRandomUnit<T>(Faction faction) where T : BaseUnit
+    {
+        return (T)_units.Where(u => u.Faction == faction).OrderBy(o => Random.value).First().UnitPrefab;
     }
 }
