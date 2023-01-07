e662e70e97ea00c910143ee71ace8d57180c16e6
groobob <349578260@gapps.yrdsb.ca>
2023-01-07 03:27:46 -0500

    Created some sprites, added offset logic for dead tiles

--- Assets/Scripts/Tile Scripts/DeadTile.cs
+++ Assets/Scripts/Tile Scripts/DeadTile.cs
@@ -4,5 +4,11 @@ using UnityEngine;
 
 public class DeadTile : Tile
 {
-    
+    [SerializeField] private Color baseColor, offsetColor;
+
+    public override void init(int x, int y)
+    {
+        var isOffset = (x + y) % 2 == 1;
+        spriteRenderer.color = isOffset ? baseColor : offsetColor;
+    }
 }
